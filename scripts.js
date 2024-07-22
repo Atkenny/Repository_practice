8964b605025a3ab148af993458d6e3248056047f
Cruz <cruzrochagisela@gmail.com>
2024-07-22 09:18:59 -0600

    Nueva función en scripts.js

--- scripts.js
+++ scripts.js
@@ -5,3 +5,12 @@ function sumar(a, b) {
 // Ejemplo de uso
 let resultado = sumar(3, 4);
 console.log("El resultado de la suma es: " + resultado); // Salida: El resultado de la suma es: 7
+
+function sumarA(a, b) {
+    return a + b;
+  }
+  
+  // Ejemplo de uso
+  let resultadoA = sumarA(3, 4);
+  console.log("El resultado de la suma es: " + resultado); // Salida: El resultado de la suma es: 7
+  
